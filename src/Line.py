3a86d41e171fd7dac2d204c4f6bd19f5f1364bfe
John Gilling <suddensleep@gmail.com>
2017-09-04 21:44:08 -0400

    Added comments to Line.py

--- src/Line.py
+++ src/Line.py
@@ -1,5 +1,10 @@
+##################################################################
+########################### LINE.PY ##############################
+######################## John Gilling ############################
+##################################################################
+
 import matplotlib.pyplot as plt
-import numpy
+
 
 class Line(object):
     """
@@ -10,22 +15,29 @@ class Line(object):
         self.a = float(a)
         self.b = float(b)
         self.c = float(c)
-        self.is_vert = self.b == 0
-        self.is_horiz = self.a == 0
+        self.is_vert = self.b == 0 # of form ax + c = 0
+        self.is_horiz = self.a == 0 # of form by + c = 0
 
         if not self.is_vert:
-            self.slope = -float(self.a) / self.b
+            self.slope = -float(self.a) / self.b 
         else:
-            self.slope = None
+            self.slope = None # undefined slope
 
     def __repr__(self):
         return "{0}*x + {1}*y + {2} = 0".format(self.a, self.b, self.c)
 
     def intersect(self, other):
+        """
+        Returns the intersection point of this Line with another.
+
+        Arguments: self, another Line object
+        Returns: Usually a point of the form [float, float], or None if the Lines are parallel.
+        """
+        
         if self.slope == other.slope:
             return None
         elif self.is_horiz:
-            return other.intersect(self)
+            return other.intersect(self) # to avoid division by zero
         else:
             det1 = (other.b*self.a - self.b*other.a)
             det2 = (other.c*self.a - self.c*other.a)
@@ -33,11 +45,25 @@ class Line(object):
             return [(-self.c - self.b*y) / self.a, y]
 
     def get_y(self, x):
+        """
+        Returns the y-value on the line at the given x-value.
+
+        Arguments: self, float
+        Returns: Usually a float, or None if the line is vertical.
+        """
+        
         if self.is_vert:
             return None
         return (-self.c - self.a*x) / self.b
         
     def plot(self, x_max, color):
+        """
+        Adds the graph of this Line to the current figure.
+
+        Arguments: self, float, matplotlib color string
+        Returns: None
+        """
+        
         xs = range(-x_max, x_max + 1)
         if self.is_vert:
             plt.plot([-self.c / self.a for x in xs], xs, color)
@@ -46,6 +72,10 @@ class Line(object):
             plt.plot(xs, ys, color)
             
 if __name__ == "__main__":
+    """
+    Test out a few simple functionalities of the Line object.
+    """
+    
     l1 = Line(1, 0, 2)
     print("l1: {}".format(l1))
     print("l1 is vertical? {}".format(l1.is_vert))
